a2f3fdc8d49d6abc2c73abfd6c8633afe6fcb296
jamalsabs <jamalsabs@gmail.com>
2022-01-30 09:07:37 -0800

    Add project files.

---
+++ HackerRankSolutionsCSharp/Algorithms.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutionsCSharp
+{
+    internal class Algorithms
+    {
+		public static int SolveMeFirst(int a, int b)
+		{
+			/*
+			 * Complete the function solveMeFirst to compute the sum of two integers.
+			 */
+
+			return a + b;
+		}
+	}
+}
---
+++ HackerRankSolutionsCSharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutionsCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"SolveMeFirst for input 3, 4 = {Algorithms.SolveMeFirst(3, 4)}");
+            
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+    }
+}
