9f3e094455b7736bc0717951b26a406611757e50
jamalsabs <jamalsabs@gmail.com>
2022-01-30 09:41:19 -0800

    Completed 3 challenges

--- HackerRankSolutionsCSharp/Algorithms.cs
+++ HackerRankSolutionsCSharp/Algorithms.cs
@@ -16,5 +16,45 @@ namespace HackerRankSolutionsCSharp
 
 			return a + b;
 		}
+
+		public static int SimpleArraySum(List<int> ar)
+        {
+			/*
+			 * For example, if the array ar =[1,2,3],1 + 2 + 3 = 6, so return 6.
+			 */
+
+			int sum = 0;
+			for (int i = 0; i < ar.Count; i++)
+				sum += ar[i];
+			return sum;
+        }
+
+		public static List<int> CompareTheTriplets(int[] a, int[] b)
+        {
+			/*
+			 * Alice and Bob each created one problem for HackerRank. A reviewer rates the two challenges,
+			 * awarding points on a scale from 1 to 100 for three categories: problem clarity, originality, and difficulty.
+			 * The rating for Alice's challenge is the triplet a = (a[0], a[1], a[2]), and the rating for Bob's challenge is the triplet b = (b[0], b[1], b[2]).
+			 * The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+			 * If a[i] > b[i], then Alice is awarded 1 point.
+			 * If a[i] < b[i], then Bob is awarded 1 point.
+			 * If a[i] = b[i], then neither person receives a point.
+			 * Comparison points is the total points a person earned.
+			 * Given a and b, determine their respective comparison points.
+			*/
+
+			List<int> result = new List<int> { 0, 0 };
+
+            for (int i = 0; i < 3; i++)
+            {
+				if (a[i] > b[i])
+					result[0]++;
+				else if (a[i] < b[i])
+					result[1]++;
+			}
+			return result;
+		}
+
+
 	}
 }
--- HackerRankSolutionsCSharp/Program.cs
+++ HackerRankSolutionsCSharp/Program.cs
@@ -10,10 +10,35 @@ namespace HackerRankSolutionsCSharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine($"SolveMeFirst for input 3, 4 = {Algorithms.SolveMeFirst(3, 4)}");
-            
+            SolveMeFirstSetup();
+            SimpleArraySetup();
+            CompareTheTripletsSetup();
+
+
+
             Console.WriteLine("Press any key to exit");
             Console.ReadLine();
+        }
+
+        public static void SolveMeFirstSetup()
+        {
+            int sum = Algorithms.SolveMeFirst(3,4);
+            Console.WriteLine($"SolveMeFirst for input 3, 4 = {sum}");
+        }
+
+        public static void SimpleArraySetup()
+        {
+            List<int> ar = new List<int>() { 1,2,3,4 };
+            int sum = Algorithms.SimpleArraySum(ar);
+            Console.WriteLine($"SimpleArraySum for input array 1,2,3,4 = {sum}");
+        }
+
+        public static void CompareTheTripletsSetup()
+        {
+            int[] alice = new int[3] { 5, 6, 7 };
+            int[] bob = new int[3] { 3, 6, 10 };
+            List<int> result = Algorithms.CompareTheTriplets(alice, bob);
+            Console.WriteLine($"CompareTheTriplets for Alice and Bob, Alice score = {result[0]} Bob score = {result[1]}");
         }
     }
 }
